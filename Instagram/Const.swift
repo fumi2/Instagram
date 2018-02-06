59b7ad1d69784dc468f64a9153818a15d4342cf1
Fumitaka Hijino <Hijino@hijino-no-macbook-puro-2017.local>
2018-02-06 09:31:11 +0900

    Firebase関連の定数を別ファイルに記述する

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by Fumitaka Hijino on 2018/02/06.
+//  Copyright © 2018年 Fumitaka Hijino. All rights reserved.
+//
+
+import Foundation
+
+struct Const {
+    static let PostPath = "posts"
+}
